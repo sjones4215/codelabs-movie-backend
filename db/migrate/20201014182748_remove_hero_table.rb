31807f82fda130a4f87686903f051cd78d4066fd
Shawn Jones <bluesfansince2013@gmail.com>
2020-10-14 13:38:23 -0500

    potential heroku fix

--- db/migrate/20201014182748_remove_hero_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveHeroTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :heros
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_175735) do
+ActiveRecord::Schema.define(version: 2020_10_14_183742) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -21,16 +21,6 @@ ActiveRecord::Schema.define(version: 2020_10_14_175735) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "body"
-    t.integer "user_id", null: false
-    t.integer "character_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["character_id"], name: "index_reviews_on_character_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,6 +34,4 @@ ActiveRecord::Schema.define(version: 2020_10_14_175735) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "reviews", "characters"
-  add_foreign_key "reviews", "users"
 end
